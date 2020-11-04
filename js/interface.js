b0095c966d56b2ca180edf19a2abf3512233a4ed
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-04 11:18:26 +0000

    interface.js file updated: temp-up and -down buttons increment temperature and temperature display accordingly, reset button
    reset button resets temp to 20 and power saving buttons allow user to switch power-saving mode on and off, temperature reacts as expeected to the mode displayed

--- js/interface.js
+++ js/interface.js
@@ -1,8 +1,35 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.currentTemperature());
-  $('#temp-up').on('click', function() {
+  updateTemperature();
+
+  $('#temp-up').click(function() {
     thermostat.up();
-    $('#temperature').text(thermostat.currentTemperature);
+    updateTemperature();
+  });
+
+  $('#temp-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temp-reset').click(function() {
+    thermostat.reset();
+    updateTemperature();
+  });
+
+  $('#psm-on').click(function() {
+    thermostat.powerSavingOn();
+    $('#power-saving-status').text('on')
+    updateTemperature();
   })
-})
+
+  $('#psm-off').click(function() {
+    thermostat.powerSavingOff();
+    $('#power-saving-status').text('off')
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.currentTemperature());
+  };
+});
